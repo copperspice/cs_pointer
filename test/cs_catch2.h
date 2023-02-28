22f57bceee4776d610ba417ef4e4a82a76746d7a
Barbara Geller <barbara@copperspice.com>
2023-02-28 09:35:05 -0800

    Catch build file, catch support files

---
+++ test/cs_catch2.h
@@ -0,0 +1,25 @@
+/***********************************************************************
+*
+* Copyright (c) 2023-2023 Barbara Geller
+* Copyright (c) 2023-2023 Ansel Sermersheim
+*
+* This file is part of CsPointer.
+*
+* CsPointer is free software, released under the BSD 2-Clause license.
+* For license details refer to LICENSE provided with this project.
+*
+* CsPointer is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*
+* https://opensource.org/licenses/BSD-2-Clause
+*
+***********************************************************************/
+
+#define CATCH_CONFIG_EXPERIMENTAL_REDIRECT
+
+#include <catch2/catch.hpp>
+
+namespace Catch {
+
+}
